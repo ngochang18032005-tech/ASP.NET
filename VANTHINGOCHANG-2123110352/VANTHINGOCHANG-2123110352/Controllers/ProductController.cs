c22aea9705a8afc322d5c63283cdcbe1f32acc1a
ngochang18032005-tech <ngochang18032005@gmail.com>
2026-03-13 08:47:06 +0700

    Add files via upload

---
+++ VANTHINGOCHANG-2123110352/VANTHINGOCHANG-2123110352/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VANTHINGOCHANG_2123110352.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
